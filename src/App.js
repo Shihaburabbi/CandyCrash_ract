d7c2b6d2aa085fd57d76b8e8608a304698e115e6
Shihaburabbi <shihaburabbi@gmail.com>
2022-05-09 18:44:25 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable no-undef */
+import React, { useState, useEffect } from "react";
+
+const width = 8;
+const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
+
+const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
+  const createBoard = () => {
+    const randomColorArrangement = [];
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+      randomColorArrangement.push(randomColor);
+    }
+    setCurrentColorArrangement(randomColorArrangement);
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  console.log(currentColorArrangement);
+
+  return (
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+          <img key={index} style={{ backgroundColor: candyColor }}
+          alt={candyColor} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
